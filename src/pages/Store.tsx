e5ed2cbd69bfd60b2c7e9fd50f8a7757ff705019
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-01-22 16:37:40 +0100

    added their picture name and price

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,15 @@
 import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
 import storeItems from "../data/items.json";
 export function Store() {
   return (
     <>
       <h1> Store </h1>
-      <Row>
+      <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map((item) => (
-          <Col>{JSON.stringify(item)}</Col>
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
         ))}
       </Row>
     </>
