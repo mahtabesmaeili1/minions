6f009c4cb5609d55d8be91bbe1ad584223e319af
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-01-22 17:03:31 +0100

    add to cart button and conditions for it

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -10,11 +10,11 @@ type StoreItemProps = {
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   const quantity = 0;
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imgUrl}
-        height="500px"
+        height="200px"
         style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
@@ -22,6 +22,11 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add To Cart</Button>
+          ) : null}
+        </div>
       </Card.Body>
     </Card>
   );
