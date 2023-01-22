f811e275c3f3f6e5ab702922ed8b92b9ea2a9e15
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-01-22 16:16:56 +0100

    showing my json file data as string yet

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,14 @@
+import { Col, Row } from "react-bootstrap";
 import storeItems from "../data/items.json";
 export function Store() {
-  return <h1> Store </h1>;
+  return (
+    <>
+      <h1> Store </h1>
+      <Row>
+        {storeItems.map((item) => (
+          <Col>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  );
 }
